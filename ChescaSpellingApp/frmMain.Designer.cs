8e6538704ff8a41006e20d97db7a6a8301cc2ced
Ferma Olay <annegaba07@yahoo.com>
2022-08-23 18:42:30 +0800

    Add project files.

---
+++ ChescaSpellingApp/frmMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace ChescaSpellingApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.lblWord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnStart.Font = new System.Drawing.Font("OldSansBlack", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Location = new System.Drawing.Point(305, 208);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(189, 85);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Zyzol Shadow", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(46, 71);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(716, 106);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Spelling Time";
+            // 
+            // txtWord
+            // 
+            this.txtWord.Font = new System.Drawing.Font("Century Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWord.Location = new System.Drawing.Point(99, 165);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.Size = new System.Drawing.Size(548, 125);
+            this.txtWord.TabIndex = 3;
+            this.txtWord.Visible = false;
+            this.txtWord.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtWord_KeyPress);
+            // 
+            // lblWord
+            // 
+            this.lblWord.AutoSize = true;
+            this.lblWord.Font = new System.Drawing.Font("Zyzol Shadow", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWord.Location = new System.Drawing.Point(87, 92);
+            this.lblWord.Name = "lblWord";
+            this.lblWord.Size = new System.Drawing.Size(225, 70);
+            this.lblWord.TabIndex = 2;
+            this.lblWord.Text = "Word:";
+            this.lblWord.Visible = false;
+            // 
+            // frmMain
+            // 
+            this.AcceptButton = this.btnStart;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(788, 418);
+            this.ControlBox = false;
+            this.Controls.Add(this.txtWord);
+            this.Controls.Add(this.lblWord);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "mainForm";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.Label lblWord;
+    }
+}
+
---
+++ ChescaSpellingApp/frmMain.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Timers;
+
+namespace ChescaSpellingApp
+{
+    public partial class frmMain : Form
+    {
+        private static System.Timers.Timer aTimer;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.Activated += AfterLoading;
+        }
+
+        private void AfterLoading(object sender, EventArgs e)
+        {
+            this.Activated -= AfterLoading;
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+            synth.Speak("Time for spelling, Chesca. Click start when you're ready.");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtWord.Visible = true;
+            lblWord.Visible = true;
+            lblTitle.Visible = false;
+            btnStart.Visible = false;
+            loadTest();
+        }
+
+        private async void loadTest()
+        {
+            int score = 0;
+            string[] words = new string[5];
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+            for (int i = 0; i < words.Length; i++)
+            {
+                var newWord = GenerateWord();
+                while (MatchedWord(words, newWord) == newWord)
+                {
+                    newWord = GenerateWord();
+                }
+                words[i] = newWord;
+
+                synth.Speak("I will give you 20 seconds to spell the word..." + words[i] + ". Again, the word is " + words[i] + ". Go.");
+                txtWord.Enabled = true;
+                await Task.Delay(20000);
+                synth.Speak("Let's see if you got the spelling correct. The correct spelling for the word " + words[i] + " is " + string.Join(", ", words[i].ToCharArray()));
+                if (words[i] == txtWord.Text.Trim())
+                    score++;
+                if (i != 4)
+                    synth.Speak("It's time for the next word.");
+                txtWord.Text = "";
+            }
+            if (score > 2)
+                synth.Speak("Good job today, Chesca. You got " + Convert.ToString(score) + " out of 5. See you again tomorrow.");
+            else
+                synth.Speak("Try harder next time, Chesca. You got " + Convert.ToString(score) + " out of 5. See you again tomorrow.");
+            Application.Exit();
+        }
+
+        private string GenerateWord()
+        {
+            string text = System.IO.File.ReadAllText(@"D:\Projects\ChescaSpellingApp\words.txt");
+
+            Random random = new Random();
+            string[] split = text.Split(' ');
+            string randomString = split[random.Next(0, split.Length)];
+            return randomString;
+        }
+
+        private string MatchedWord(string[] arr, string word)
+        {
+            if (arr[0] == null)
+                return String.Empty;
+            return Array.Find(arr, element => element == word);
+        }
+
+        private void txtWord_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back);
+        }
+    }
+}
